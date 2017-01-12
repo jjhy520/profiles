757647d7e43eb0f671f9c3085c1e7f16d701acc6
jjhy520 <jjhy520@gmail.com>
2017-01-12 19:24:55 +0800

    Delete update-profilesRenFile.bat

--- chrome/Batch/update-profilesRenFile.bat
+++
@@ -1,5 +0,0 @@
-::github.com不支持中文名跟@，需下载后另外修改。
-@Echo Off
-cd /d %~dp0
-ren ..\..\profiles\extensions\userChromeJS_Mix-develop.com userChromeJS_Mix@develop.com
-ren ..\..\profiles\gm_scripts\HTML5 优土豆HTML5播放
