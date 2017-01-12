dddb837d6ca220a0545d90d1cb5c7d662e3885bd
jjhy520 <jjhy520@gmail.com>
2017-01-12 11:08:00 +0800

    Update update-profiles.bat

--- chrome/Batch/update-profiles.bat
+++ chrome/Batch/update-profiles.bat
@@ -1,3 +1,8 @@
 @Echo Off
 cd /d %~dp0
-wget.exe -N --no-check-certificate https://github.com/jjhy520/profiles/chrome/Batch/archive/master.zip
+wget.exe -N --no-check-certificate https://github.com/jjhy520/profiles/archive/master.zip
+7z.exe x master.zip -y
+ren profiles-master profiles
+xcopy "profiles" ..\..\ /s /y /i
+rd "profiles-master" /s/q
+del "master.zip"
