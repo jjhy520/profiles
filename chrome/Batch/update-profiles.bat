da9ab861f612ee2d367c524b5d6904546a50009f
jjhy520 <jjhy520@gmail.com>
2017-01-12 17:53:34 +0800

    Update update-profiles.bat

--- chrome/Batch/update-profiles.bat
+++ chrome/Batch/update-profiles.bat
@@ -3,8 +3,8 @@ cd /d %~dp0
 rd "profiles" /s/q
 wget.exe -N --no-check-certificate https://github.com/jjhy520/profiles/archive/master.zip
 7z.exe x master.zip -y
-ren profiles\extensions\userChromeJS_Mix-develop.com userChromeJS_Mix@develop.com
 ren profiles-master profiles
+profiles\chrome\Batch\update-profilesRenFile.bat
 xcopy "profiles" ..\..\ /s /y /i
 rd "profiles" /s/q
 del "master.zip"
