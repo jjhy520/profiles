fde988ce4f40a8567cdd66565bb6660e3cf092b6
jjhy520 <jjhy520@gmail.com>
2017-01-12 13:14:43 +0800

    Delete update-profiles.bat

--- chrome/Batch/update-profiles.bat
+++
@@ -1,9 +0,0 @@
-@Echo Off
-cd /d %~dp0
-rd "profiles" /s/q
-wget.exe -N --no-check-certificate https://github.com/jjhy520/profiles/archive/master.zip
-7z.exe x master.zip -y
-ren profiles-master profiles
-xcopy "profiles" ..\..\ /s /y /i
-rd "profiles" /s/q
-del "master.zip"
