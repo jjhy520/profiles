3bfbd762bc7f7bf5997a88d6a94ffd89dac34486
jjhy520 <jjhy520@gmail.com>
2017-01-12 19:31:11 +0800

    Delete player.js

--- extensions/userChromeJS_Mix-develop.com/content/m3u8/player.js
+++
@@ -1,4 +0,0 @@
-(() => {
-  let video = document.getElementById('video'), hls = new Hls(), url = location.search.substr(1);
-  hls.loadSource(url), hls.attachMedia(video), hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
-})()
