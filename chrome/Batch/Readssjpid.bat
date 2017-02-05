a207db2f6a87809d526453bbac85334b916b0323
jjhy520 <jjhy520@gmail.com>
2017-02-05 18:39:12 +0800

    Update Readssjpid.bat

--- chrome/Batch/Readssjpid.bat
+++ chrome/Batch/Readssjpid.bat
@@ -19,7 +19,7 @@ var getContent = function(body){
 }
 
 var http = new ActiveXObject('MSXML2.XMLHTTP');
-http.open('GET', 'http://www.ishadow.pw/', false);
+http.open('GET', 'http://www.ishadow.site/', false);
 http.send();
 
 var a = getContent(http.responseBody);
