b31a156fba6fa69ac492f71cf6f349c5e43f81c3
jjhy520 <jjhy520@gmail.com>
2017-02-13 23:51:23 +0800

    Update back-SSID.bat

--- chrome/Batch/back-SSID.bat
+++ chrome/Batch/back-SSID.bat
@@ -1,3 +1,10 @@
 @Echo Off
 cd /d %~dp0
-copy /y "gui-config.jsonbak" gui-config.json
+copy /y "gui-config.jsonbak5" gui-config.jsonbak4
+copy /y "gui-config.jsonbak4" gui-config.jsonbak3
+copy /y "gui-config.jsonbak3" gui-config.jsonbak2
+copy /y "gui-config.jsonbak2" gui-config.jsonbak1
+copy /y "gui-config.jsonbak1" gui-config.json
+taskkill /f /im Shadowsocks.exe
+start Shadowsocks.exe
+exit
