b5d762b63f30b57457cb939072113764151f03ed
jjhy520 <jjhy520@gmail.com>
2017-02-13 22:52:47 +0800

    Update Readssusid.bat

--- chrome/Batch/Readssusid.bat
+++ chrome/Batch/Readssusid.bat
@@ -19,7 +19,7 @@ var getContent = function(body){
 }
 
 var http = new ActiveXObject('MSXML2.XMLHTTP');
-http.open('GET', 'http://www.ishadow.pro/', false);
+http.open('GET', 'http://160.16.231.71/', false);
 http.send();
 
 var a = getContent(http.responseBody);
